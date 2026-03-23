3a3aa430e7978cb699ed22a3773df347e34b7503
MEIRG <meirg2001@gmail.com>
2026-03-23 16:55:45 +0200

    add the gps location to init file

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,5 +1,5 @@
 from .config import settings 
 from .kafka import Consumer, Producer, Topics 
-from .models import VehicleTelemetry, AlertAction, AlertEvent, ProcessedVehicleData, RiskLevel
+from .models import VehicleTelemetry, AlertAction, AlertEvent, ProcessedVehicleData, RiskLevel, GPSLocation
 from .redis import RedisClient 
 from .utils import  get_logger
